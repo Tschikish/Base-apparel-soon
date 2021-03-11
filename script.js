2f9cdaa8c67e50a32f50ec475a73c5298bc340e2
Milutin Pavlovic <pavlovicdmilutin@gmail.com>
2021-03-11 02:51:43 +0100

    small edit in code

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const form = document.getElementById('form');
 const email = document.getElementById('email');
 
+/*
+Testing area
+const a = 15;
+*/
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const emailVal = email.value;
 
     //check if it's valid
-    if (!validateEmail(emailVal)) {
+    if (validateEmail(emailVal)) {
         form.classList.add('error');
     } else {
         form.classList.remove('error');
@@ -16,5 +21,6 @@ form.addEventListener('submit', (e) => {
 
 function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var test = 'git testing string';
     return re.test(String(email).toLowerCase());
 }
